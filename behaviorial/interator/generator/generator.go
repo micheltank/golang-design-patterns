bed28ffb6ebea914a83c2a44245540c9ad060d76
Michel Tank <micheltank@outlook.com>
2021-10-31 20:18:46 -0300

    iterator

---
+++ behaviorial/interator/generator/generator.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	FirstName, MiddleName, LastName string
+}
+
+func (p *Person) NamesGenerator() <-chan string {
+	out := make(chan string)
+	go func() {
+		defer close(out)
+		out <- p.FirstName
+		if len(p.MiddleName) > 0 {
+			out <- p.MiddleName
+		}
+		out <- p.LastName
+	}()
+	return out
+}
+
+func main() {
+	p := Person{"Alexander", "Graham", "Bell"}
+	for name := range p.NamesGenerator() {
+		fmt.Println(name)
+	}
+}
